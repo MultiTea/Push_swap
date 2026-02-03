e039b73babe342c8906aec6eff69b52ad528ebc6
MultiTea <lucasbolea@live.fr>
2026-02-03 22:41:55 +0100

    Cleaned up headers and added a synced todo list

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: lbolea <lbolea@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 18:19:13 by lbolea            #+#    #+#             */
-/*   Updated: 2026/02/03 16:34:38 by lbolea           ###   ########.fr       */
+/*   Updated: 2026/02/03 22:35:40 by lbolea           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,32 +20,26 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-// ――― INPUT HANDLING ――― //
-/* Parsing */
+/// ――― INPUT HANDLING ――― ///
+// Parsing
 int					*parsing(int argc, char *argv[], int *len);
 char				**ft_split_args(char const *s);
-/* Libft */
-char				*ft_strjoin(char const *s1, char const *s2);
-/* Utils */
-// General
+// Utils
+/*General*/
 void				free_arr(char **array, size_t position);
 
 long				ft_atol(const char *nptr);
-//	Parsing
+/*Parsing*/
 int					ft_check(int argc, char **argv);
 int					ft_check_str(char *argv);
 int					is_empty(char *str);
 int					is_dup(int argc, char **argv);
 
-// ――― ERROR HANDLING ――― //
-/* General */
+/// ――― ERROR HANDLING ――― ///
 void				*ft_error(void);
-/* Libft */
-void				ft_putendl_fd(char *s, int fd);
 
-// ――― STACK MANAGEMENT ――― //
-
-/* Structure */
+/// ――― STACK MANAGEMENT ――― ///
+// Structure
 typedef struct s_node
 {
 	int				data;
@@ -55,30 +49,36 @@ typedef struct s_node
 	bool			median;
 	bool			cheapest;
 
-	struct s_node	*head;
-	struct s_node	*next;
-	struct s_node	*previous;
-}					t_node;
+	struct s_node	*target;
 
-t_node				stack_init(unsigned int capacity);
+	struct s_node	*next;
+	struct s_node	*prev;
+}					t_node;
+// Structure management
+t_node				*new_node(int content);
 int					isEmpty(t_node stack);
 
-// ――― OPERATIONS ――― //
-/* Swap */
+/// ――― OPERATIONS ――― ///
+// Swap
+void				swap(t_node **stack);
 void				sa(t_node a);
 void				sb(t_node b);
 void				ss(t_node a, t_node b);
-/* Push */
+// Push
+void				push(t_node **stack);
 void				pa(t_node a, t_node b);
 void				pb(t_node b, t_node a);
-/* Rotate */
+// Rotate
+void				rotate(t_node **stack);
 void				ra(t_node a);
 void				rb(t_node b);
 void				rr(t_node a, t_node b);
+// Reverse rotate
+void				reverse_rotate(t_node **stack);
 void				rra(t_node a);
 void				rrb(t_node b);
 void				rrr(t_node a, t_node b);
 
-// ――― ALGORITHM ――― //
+/// ――― ALGORITHM ――― ///
 
 #endif
---
+++ todo.md
@@ -0,0 +1,62 @@
+# PUSH_SWAP
+## PARSING
+The program must work with several numerical arguments
+- [x] ./push_swap 1 3 5 +9 20 -4 50 60 04 08
+
+The program also works if you receive a single character list as a parameter
+- [x] ./push_swap "3 4 6 8 9 74 -56 +495"
+
+The program should NOT work if it encounters another character
+- [x] ./push_swap 1 3 dog 35 80 -3
+- [x] ./push_swap a
+- [x] ./push_swap 1 2 3 5 67b778 947
+- [x] ./push_swap " 12 4 6 8 54fhd 4354"
+- [x] ./push_swap 1 --    45 32
+        these examples should return "Error\n"
+
+The program should NOT work if it encounters a double number
+
+- [x] ./push_swap 1 3 58 9 3
+- [x] ./push_swap 3 03
+- [x] ./push_swap " 49 128     50 38   49"
+    these examples should return "Error\n"
+- [x] ./push_swap "95 99 -9 10 9"
+        this example should work because -9 & 9 are not equal
+
+The program should work with INT MAX & INT MIN
+- [x] ./push_swap 2147483647 2 4 7
+- [x] ./push_swap 99 -2147483648 23 545
+- [x] ./push_swap "2147483647 843 56544 24394"
+    these examples should work and sort your list
+- [x] ./push_swap 54867543867438 3
+- [x] ./push_swap -2147483647765 4 5
+- [x] ./push_swap "214748364748385 28 47 29"
+    these examples should return "Error\n"
+
+Nothing has been specified when strings and int are mixed. It's up to you what you want to do
+- [x] ./push_swap "1 2 4 3" 76 90 "348 05
+---
+## STRUCTURE
+### Step 1
+[Linked list lesson](https://code-vault.net/course/z1ry7bakhs:1603732279805)
+- [x] Create `t_list`
+- [ ] Initialize and add nodes to `t_list`
+- [ ] Pass the array of `int` to the structure
+- [ ] Check for leaks and if double-linked
+- [ ] Implement the moves (use generic functions)
+---
+## ALGORITHM
+### Step 2
+- [ ] Sort 3 numbers
+- [ ] Sort 5 numbers
+### Step 3
+- [ ] Push A to B
+- [ ] Target decision (Greedy phase)
+  - [ ] Find cheapest argument
+- [ ] Optimize moves
+- [ ] Rotate A to sort from smallest to biggest
+### Step 4
+- [ ] Verify `norminette`
+- [ ] Check leaks
+- [ ] Check and clean repo for forbidden files
+---
